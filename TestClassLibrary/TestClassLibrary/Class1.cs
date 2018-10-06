f31783283ae9c3abd8071f1d824bca40de11cda9
Mark Hingston <mark@bitshift.cc>
2018-10-06 16:07:56 +0100

    first commit

---
+++ TestClassLibrary/TestClassLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace TestClassLibrary
+{
+    public class Class1
+    {
+        public static string Method1()
+        {
+            return new JObject(
+                new JProperty("status", "ok")
+            ).ToString();
+        }
+    }
+}
---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClassLibrary.Class1.Method1(); // System.IO.FileNotFoundException: 'Could not load file or assembly 'Newtonsoft.Json, Version=11.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed' or one of its dependencies. The system cannot find the file specified.'
+        }
+    }
+}
